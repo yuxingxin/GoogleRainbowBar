93383783b43ecf88aaadf8e87349cdeed0cf75a9
sean <lx8909@gmail.com>
2015-05-24 15:44:41 +0800

    add androidTest

--- app/src/androidTest/java/com/yuxingxin/googlerainbowbar/ApplicationTest.java
+++ app/src/androidTest/java/com/yuxingxin/googlerainbowbar/ApplicationTest.java
@@ -2,7 +2,6 @@ package com.yuxingxin.googlerainbowbar;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
-
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
